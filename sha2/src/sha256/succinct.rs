8c737f18833d9ac679879ba9b0d4edad943ce0c8
John Guibas <john@succinct.xyz>
2024-01-16 19:39:11 -0800

    feat: sha256 fork

--- sha2/src/sha256/succinct.rs
+++ sha2/src/sha256/succinct.rs
@@ -8,14 +8,17 @@ pub fn compress(state: &mut [u32; 8], blocks: &[[u8; 64]]) {
     unsafe {
         for i in 0..blocks.len() {
             let mut w = [0u32; 64];
-            for j in 0..16 {
-                w[j] = u32::from_be_bytes([
-                    blocks[i][j * 4],
-                    blocks[i][j * 4 + 1],
-                    blocks[i][j * 4 + 2],
-                    blocks[i][j * 4 + 3],
-                ]);
-            }
+            // let mut w = [0u32; 64];
+            // for j in 0..16 {
+            //     w[j] = u32::from_be_bytes([
+            //         blocks[i][j * 4],
+            //         blocks[i][j * 4 + 1],
+            //         blocks[i][j * 4 + 2],
+            //         blocks[i][j * 4 + 3],
+            //     ]);
+            // }
+            let mut chunk: [u32; 16] = unsafe { std::mem::transmute(blocks[i]) };
+            w[0..16].copy_from_slice(&chunk);
             syscall_sha256_extend(w.as_mut_ptr());
             syscall_sha256_compress(w.as_mut_ptr(), state.as_mut_ptr());
         }
