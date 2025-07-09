027ce2eb6c3ff3f94108e5e57c1febdb73d00c90
Tej Qu Nair <tej.q.nair@gmail.com>
2025-07-09 19:32:31 +0000

    sha2 0.10.8 v6 patch

--- sha2/src/sha256/succinct.rs
+++ sha2/src/sha256/succinct.rs
@@ -1,23 +1,23 @@
 extern "C" {
-    fn syscall_sha256_extend(w: *mut u32);
-    fn syscall_sha256_compress(w: *mut u32, state: *mut u32);
+    fn syscall_sha256_extend(w: *mut [u64; 64]);
+    fn syscall_sha256_compress(w: *mut [u64; 64], state: *mut [u64; 8]);
 }
 
 #[inline]
 pub fn compress(state: &mut [u32; 8], blocks: &[[u8; 64]]) {
+    use core::convert::TryInto;
     unsafe {
-        for i in 0..blocks.len() {
-            let mut w = [0u32; 64];
-            for j in 0..16 {
-                w[j] = u32::from_be_bytes([
-                    blocks[i][j * 4],
-                    blocks[i][j * 4 + 1],
-                    blocks[i][j * 4 + 2],
-                    blocks[i][j * 4 + 3],
-                ]);
+        for block in blocks {
+            let mut w = [0u64; 64];
+            for (o, chunk) in w.iter_mut().zip(block.chunks_exact(4)) {
+                *o = u32::from_be_bytes(chunk.try_into().unwrap()) as u64;
             }
-            syscall_sha256_extend(w.as_mut_ptr());
-            syscall_sha256_compress(w.as_mut_ptr(), state.as_mut_ptr());
+            let mut state_u64 = state.map(|x| x as u64);
+            syscall_sha256_extend(&mut w);
+            syscall_sha256_compress(&mut w, &mut state_u64);
+            for (l, r) in core::iter::zip(&mut *state, state_u64) {
+                *l = r as u32;
+            }
         }
     }
 }
